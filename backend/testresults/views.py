bb215aeaa0ecb4d6fa7a26cdb9956c46fb287e18
Oskru <oskrupaa@gmail.com>
2022-09-11 01:04:02 +0200

    Add TodoView view

--- backend/testresults/views.py
+++ backend/testresults/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TestresultSerializer
+from .models import Testresult
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TestresultSerializer
+    queryset = Testresult.objects.all()
