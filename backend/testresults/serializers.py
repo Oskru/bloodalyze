a51b3117d43e5247507b2b07633c7ee3fe9005e9
Oskru <oskrupaa@gmail.com>
2022-09-11 00:57:24 +0200

    Add testresults serializers

---
+++ backend/testresults/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Testresult
+
+class TodoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Testresult
+        fields = ('id', 'date_added', 'rbc_level', 'hgb_level', 'hct_level', 'mcv_level', 'mch_level', 'mchc_level', 'wbc_level', 'plt_level')
