fe51a677320b79cc5f6b4d3808a8b14f9ec083bd
Oskru <oskrupaa@gmail.com>
2022-09-11 00:06:23 +0200

    Add TestresultsAdmin registration

--- backend/testresults/admin.py
+++ backend/testresults/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Testresults
 
 # Register your models here.
+
+class TestresultsAdmin(admin.ModelAdmin):
+    list_display = ('date',)
+
+admin.site.register(Testresults, TestresultsAdmin)
