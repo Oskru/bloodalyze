d802b1c0d67daa34f64f704515f420f59768f5ac
Oskru <oskrupaa@gmail.com>
2022-09-11 13:05:03 +0200

    Add test_date field to Testresult model

--- backend/testresults/models.py
+++ backend/testresults/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Testresult(models.Model):
 
     date_added = models.DateField(auto_now_add=True)
+    test_date = models.DateField(null=False)
     rbc_level = models.DecimalField(max_digits=10, decimal_places=2)
     hgb_level = models.DecimalField(max_digits=10, decimal_places=2)
     hct_level = models.DecimalField(max_digits=10, decimal_places=2)
