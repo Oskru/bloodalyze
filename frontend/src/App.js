d0a7226d977cdbd5f1a88ecb456a3d1306177c97
Oskru <oskrupaa@gmail.com>
2022-10-07 12:10:00 +0200

    Add basic frontend pattern components
    Add components: Navbar, Home, Loginform, Registerform, Footer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Loginform from './components/loginform/Loginform';
+import Registerform from './components/registerform/Registerform';
+import Footer from './components/footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import BackgroundImage from './static/bg.jpg';
+
+const styles = {
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    fontFamily: 'Nunito',
+    overflow: 'hidden',
+  }
+};
 
 function App() {
+  
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.root}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/login' element={<Loginform />}></Route>
+        <Route exact path='/register' element={<Registerform />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
