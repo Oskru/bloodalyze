c9afc37e5c8e8cd27a6f007b652729f41b9ed883
Oskru <oskrupaa@gmail.com>
2022-10-30 16:15:13 +0100

    Add a root of backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connection = require('./db');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+// Database connection
+connection();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Auth
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+
+// Launch server
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
