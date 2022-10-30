3643d6f0dc4058b90ff08ff62d6b12be30a327c0
Oskru <oskrupaa@gmail.com>
2022-10-30 16:17:30 +0100

    Add user registration with database

---
+++ backend/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this.id}, process.env.JWTPRIVATEKEY, {expiresIn: '7d'});
+    return token
+};
+
+const User = mongoose.model('user', userSchema)
+
+const complexityOptions = {
+    min: 8,
+    max: 30,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 0,
+};
+
+const validate = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required().label('Email'),
+        firstName: Joi.string().required().label('First name'),
+        lastName: Joi.string().required().label('Last name'),
+        password: passwordComplexity(complexityOptions).required().label('Password')
+    });
+    return schema.validate(data)
+}
+
+module.exports = {User, validate}
---
+++ backend/routes/users.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {User, validate} = require('../models/user');
+const bcrypt = require('bcrypt');
+
+router.post('/', async(req, res) => {
+    try {
+        const {error} = validate(req.body);
+        if (error)  
+            return res.status(400).send({message: error.details[0].message})
+        
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user)
+       
+        return res.status(409).send({message: 'User with given email already exist!'})
+        
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        await new User({...req.body, password: hashPassword}).save();
+        res.satuts(201).send({message: 'User created successfully'});
+    
+    
+    } catch (error) {
+        res.status(500).send({message: `Internal server error`});
+    }
+});
+
+module.exports = router;
