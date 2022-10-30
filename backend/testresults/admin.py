43274ad309e9f7388c504165053539ad2d4f2dc0
Oskru <oskrupaa@gmail.com>
2022-10-30 13:06:08 +0100

    Delete Django backend

--- backend/testresults/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Testresult
-
-# Register your models here.
-
-class TestresultsAdmin(admin.ModelAdmin):
-    list_display = ('date_added',)
-
-admin.site.register(Testresult, TestresultsAdmin)
--- backend/testresults/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Testresult
-
-class TestresultSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Testresult
-        fields = ('id', 'date_added', 'rbc_level', 'hgb_level', 'hct_level', 'mcv_level', 'mch_level', 'mchc_level', 'wbc_level', 'plt_level')
